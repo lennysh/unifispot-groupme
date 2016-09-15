249e58b0a81d1495371ba125991f92b4068f00da
Lenny Shirley <ircmaster@hotmail.com>
2016-09-15 13:36:31 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# GroupMe Config
+url = 'https://api.groupme.com/v3/bots/post'
+access_token = '' #Access Token from GroupMe Dev Page
+headers = {'content-type': 'application/json'}
+botid = '' #BotID from GroupMe Bots Page
+
+# MySQL DB Config for Unifispot
+dbhost = 'localhost'
+dbuser = 'root'
+dbpass = '<password>'
+dbname = 'poppet'
